fb4f75b4c7e65391ee6a240a324eaee5e20eb8fc
Tushar Taneja <tushartaneja24@gmail.com>
2023-07-04 19:02:25 +0530

    Initialized Project

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb://0.0.0.0:27017/react-login-tut")
+mongoose.connect("mongodb://127.0.0.1:27017/RegistrationsData")  //mongodb://127.0.0.1:27017/RegistrationsData
 .then(()=>{
     console.log("mongodb connected");
 })
